02944cbe5f332d5edd3c981782209a601629a4d5
Gvaireth <pzajaczkowski@gmail.com>
2016-08-01 07:57:02 +0200

    ui-grid with working columnDefs

--- src/main/webapp/script/app.js
+++ src/main/webapp/script/app.js
@@ -19,14 +19,21 @@ endoMinion.factory('endoService', function($http, $rootScope) {
 
 endoMinion.controller('workoutsCtrl', function($scope, endoService) {
 	
-	/*$scope.gridOptions = { 
-            columnDefs: [{ field: 'id', displayName: 'no', width: 90 },
-                         { field: 'readableStartTime', displayName: 'Start time', width: 80 }]}*/
+	$scope.gridOptions = { data : 'workouts',
+			headerRowHeight: 130,
+            columnDefs: [{ field: 'id', displayName: 'no.', width: '50' },
+                         { field: 'readableStartTime', displayName: 'Start time', width: '150' },
+                         { field: 'readableDuration', displayName: 'Duration', width: '100' },
+                         { field: 'durationRank', displayName: 'Duration#', width: '100' },
+                         { field: 'distance', displayName: 'Distance', width: '100' },
+                         { field: 'distanceRank', displayName: 'Distance#', width: '100' },
+                         { field: 'calories', displayName: 'Calories', width: '100' },
+                         { field: 'speedAvg', displayName: 'Avg speed', width: '100' },
+                         { field: 'speedAvgRank', displayName: 'Avg speed#', width: '100' },
+                         { field: 'sport', displayName: 'Activity', width: '120' },
+            ]}
 	
-    $scope.gridOptions = {
-    	      enableColumnResizing: true
-    	    };
-	
+
 	$scope.status='Loading Workouts...';
 	endoService.getSimpleData().then(function(result) {
 		$scope.serverResponse = result.data;
