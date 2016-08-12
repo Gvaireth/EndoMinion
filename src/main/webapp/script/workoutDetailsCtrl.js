559fa28a14a1a8a65ee81d543a642eeb072b1f36
gvaireth <pzajaczkowski@gmail.com>
2016-08-12 08:54:55 +0200

    workout data displayed in details, tracking resolution calculation

--- src/main/webapp/script/workoutDetailsCtrl.js
+++ src/main/webapp/script/workoutDetailsCtrl.js
@@ -1,12 +1,21 @@
-endoMinion.controller('workoutDetailsCtrl', function($scope,$location,$rootScope, endoService) {
+endoMinion.controller('workoutDetailsCtrl', function($scope, $location,
+		$rootScope, endoService) {
 	$scope.status = 'Loading Workout Details...';
-	$scope.id=$location.search().id;
-	
-	
-	endoService.getDetailedWorkout($rootScope.currentWorkout.endomondoId).then(function(result) {
-		$scope.details = result.data;
-		$scope.status = '';
-		$scope.pointsNumber=result.data.points.length;
-		$scope.workout=$rootScope.currentWorkout;
-	})
+	$scope.id = $location.search().id;
+
+	endoService.getDetailedWorkout($rootScope.currentWorkout.endomondoId).then(
+			function(result) {
+				$scope.details = result.data;
+				$scope.status = '';
+				$scope.pointsNumber = result.data.points.length;
+				$scope.workout = $rootScope.currentWorkout;
+				if ($scope.pointsNumber != 0) {
+					$scope.workout.trackingResolution = $scope.workout.distance
+							* 1000 / $scope.pointsNumber;
+					$scope.workout.trackingResolution=$scope.workout.trackingResolution.toFixed(2);
+					$scope.workout.trackingResolution+=' m';
+				} else {
+					$scope.workout.trackingResolution = 'N/A';
+				}
+			})
 });
