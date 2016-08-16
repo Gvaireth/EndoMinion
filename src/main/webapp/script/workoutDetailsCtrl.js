f40ae00e68b485bb43f2e8583ffee62268980b37
gvaireth <pzajaczkowski@gmail.com>
2016-08-16 07:36:48 +0200

    ngMap removed

--- src/main/webapp/script/workoutDetailsCtrl.js
+++ src/main/webapp/script/workoutDetailsCtrl.js
@@ -1,13 +1,8 @@
-endoMinion.controller('workoutDetailsCtrl', function($scope, $routeParams,endoService,NgMap) {
+endoMinion.controller('workoutDetailsCtrl', function($scope, $routeParams,endoService) {
 	$scope.status = 'Loading Workout Details...';
 	endoService.getAccountInfo().then(function(result) {
 		$scope.account = result.data;
 	});
-	  NgMap.getMap().then(function(map) {
-		    console.log(map.getCenter());
-		    console.log('markers', map.markers);
-		    console.log('shapes', map.shapes);
-		  });
 
 	endoService.getDetailedWorkout($routeParams.endomondoId).then(
 			function(result) {
